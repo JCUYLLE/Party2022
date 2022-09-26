40d81ec39ca825f4c718c72262cda0fa9ffc37e5
robbe <robbecuylle@gmail.com>
2022-09-26 21:18:29 +0200

    1

--- src/main/java/be_buschop/ap2022/party/controllers/Homecontroller.java
+++ src/main/java/be_buschop/ap2022/party/controllers/Homecontroller.java
@@ -2,11 +2,14 @@ package be_buschop.ap2022.party.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.ui.Model;
 @Controller
+
 public class Homecontroller {
+    private final int myspecialnumber = 35;
     @GetMapping(value = {"/home", "/"})
-    public String home (){
+    public String home (Model model){
+        model.addAttribute("myspecialnumber",myspecialnumber);
         return "home";
     }
 
