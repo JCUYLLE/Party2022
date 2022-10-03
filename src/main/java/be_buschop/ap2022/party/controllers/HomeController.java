93824552fd09fdb172bab22950e93109d6a3655c
robbe <robbecuylle@gmail.com>
2022-10-03 19:52:51 +0200

    1

---
+++ src/main/java/be_buschop/ap2022/party/controllers/HomeController.java
@@ -0,0 +1,34 @@
+package be_buschop.ap2022.party.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+@Controller
+public class HomeController {
+    private final int mySpecialNumber = 35;
+    @GetMapping(value = {"/", "/home", "/home/"})
+
+    public String home(Model model) {
+        model.addAttribute( "mySpecialNumber", mySpecialNumber );
+        return "home";
+    }
+
+    @GetMapping(value = {"/venuelist", "/venuelist/"})
+
+    public String venuelist(){
+        return "venuelist";
+    }
+
+    @GetMapping("/about")
+
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping(value = {"/venuedetails/{venuename}"})
+    public String venuedetails(Model model, @PathVariable String venuename){
+        model.addAttribute("venuename",venuename);
+        return "venuedetails";
+    }
+}
